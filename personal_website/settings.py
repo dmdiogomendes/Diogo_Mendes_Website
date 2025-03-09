385e727dbba5742875eef712b7247189a3c41595
Diogo Mendes <diogoafm6@gmail.com>
2025-03-09 16:32:12 +0000

    commit files

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pj8g7u8^70_us(xkva3$9^sr%3la1se%6&jras3^(7i+12t*6k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-8eac.up.railway.app']
 
 
 # Application definition
